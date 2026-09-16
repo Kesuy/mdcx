8dd4242c7222cfa0b13182bcc6e3c52266161c5a
xshare.cc <nicbest@163.com>
2026-09-17 07:46:30 +0800

    fix: normalize settings polish import block

--- mdcx/controllers/main_window/settings_layout_polish.py
+++ mdcx/controllers/main_window/settings_layout_polish.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QGridLayout, QHBoxLayout, QLabel, QLayout, QWidget
 
-
 _ALIGNMENT = Qt.AlignmentFlag.AlignVCenter
 
 
